4fe61d4f67d8467ffd77c5d1d8b9a73ed1a2f2fc
Aravind0424 <165820760+Aravind0424@users.noreply.github.com>
2024-04-09 17:09:34 +0530

    Delete read_list(n_elements).py

--- read_list(n_elements).py
+++
@@ -1,5 +0,0 @@
-# number of elements
-n = int(input("Enter number of elements : "))
-# Below line read n inputs for list from user using map() function
-a = list(map(int, input("Enter the numbers : ").strip().split()))[:n]
-print("List:", a)
